c52250baa6ef265ff9ed4305b26ffdc3fd3f3f52
KTibow <KTibow@users.noreply.github.com>
2025-09-24 15:19:47 -0700

    great

--- sdk/src/lib/index.ts
+++ sdk/src/lib/index.ts
@@ -1,3 +1,3 @@
-export { getLogin, getStorage } from "./storage.js";
+export { getLoginRecognized, getStorage } from "./storage.js";
 export * from "./trackready.js";
 export { encode, decode } from "./utils-base36.js";
