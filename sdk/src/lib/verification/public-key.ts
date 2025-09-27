0d30b684e4e9b616bc8a4135808895a1895ffff1
KTibow <KTibow@users.noreply.github.com>
2025-09-27 14:26:10 -0700

    new key

--- sdk/src/lib/verification/public-key.ts
+++ sdk/src/lib/verification/public-key.ts
@@ -1,5 +1,5 @@
 export default `-----BEGIN PUBLIC KEY-----
-MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAElBpHf+hSvIo/+3xhm1Q6sMqN/uXf
-3dZX4dXYPtZMij+m3S6g1MSzxMhTquTbdikQmD1RmKoDcTC4Zxdj4VMhjg==
+MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAERXBMZBkx95Iua82tR9gsmdegOwuB
+OGgbkB6h9s4DROhBRkpubcAiu5Qa9GizbWk1g0RwrnrdVo7yxENcEs2+zw==
 -----END PUBLIC KEY-----
 `;
