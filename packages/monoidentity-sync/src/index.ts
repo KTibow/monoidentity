2fb7b83bfed14fda13a357e0337474b3087373e2
KTibow <KTibow@users.noreply.github.com>
2026-02-25 19:41:01 -0800

    also export Bucket

--- packages/monoidentity-sync/src/index.ts
+++ packages/monoidentity-sync/src/index.ts
@@ -6,6 +6,7 @@ import { localPush } from './backuplocal-push';
 import { KEY_CLOUD, KEY_LOCAL_ENABLED } from './utils-config';
 
 export { default as createBucket } from './bucket-create.remote';
+export type { Bucket } from './types';
 
 export { cloudPull } from './backupcloud-pull';
 export { localPull } from './backuplocal-pull';
